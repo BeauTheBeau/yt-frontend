fcdf06e7c712798595b2b700fa6de6e3be58b585
beauthebeau <beau@beauthebeau.pro>
2023-12-16 00:40:42 +0000

    feat: Allow users to change theme in the navbar.hbs

--- public/javascript/theme.js
+++ public/javascript/theme.js
@@ -1,19 +1,35 @@
 const html = document.querySelector('html');
 
+function setIcon(newTheme) {
+
+    const themeSwitcher = document.querySelector('#theme-toggle');
+    const icon = themeSwitcher.querySelector('i');
+
+    if (newTheme === 'dark') {
+        icon.classList.add('nf-md-lightbulb_off_outline');
+        icon.classList.remove('nf-md-lightbulb');
+    }
+    else {
+        icon.classList.add('nf-md-lightbulb');
+        icon.classList.remove('nf-md-lightbulb_off_outline');
+    }
+
+}
+
 function toggleTheme() {
     if (html.dataset.theme === 'dark') html.dataset.theme = 'light';
     else html.dataset.theme = 'dark';
     document.cookie = `theme=${html.dataset.theme}; path=/; max-age=31536000; SameSite=Strict`;
+    setIcon(html.dataset.theme);
 }
 
 function setDefaultTheme() {
-
-
 
     const preference = window.matchMedia('(prefers-color-scheme: dark)')
 
     html.dataset.theme = preference.matches ? 'dark' : 'light';
     document.cookie = `theme=${html.dataset.theme}; path=/; max-age=31536000; SameSite=Strict`;
+    setIcon(html.dataset.theme);
 }
 
 if (!document.cookie) setDefaultTheme();
