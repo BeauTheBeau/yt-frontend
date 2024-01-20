58eda01b7c67c7121f76d9e8db522a92fa4e27a4
beauthebeau <beauthebeau@proton.me>
2024-01-20 01:34:05 +0000

    feat: Allow users to search videos on YouTube

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const config = require('../config.json')
+const fs = require('fs');
+const youtubedl = require("youtube-dl-exec");
+const Fuse = require('fuse.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
     // Get all videos in the videos/ directory
     const fs = require('fs');
@@ -18,11 +21,11 @@ router.get('/', function(req, res, next) {
         videoData.push(metadata);
     });
 
-    res.render('index', {res, title: `${res.locals.site.title} | Home`, videos: videoData });
+    res.render('index', {res, title: `${res.locals.site.title} | Home`, videos: videoData});
 });
 
 /* GET watch page - this is where the user can watch videos */
-router.get('/watch', function(req, res, next) {
+router.get('/watch', function (req, res, next) {
 
     // Parameters
     // - ?v = the video URL / ID
@@ -37,7 +40,12 @@ router.get('/watch', function(req, res, next) {
             const domain = config.server.domain;
 
             if (!metadata.downloaded) return res.redirect(`/newVideo?v=${videoId}`);
-            else return res.render('watch', {title: `${metadata.title} | ${res.locals.site.title}`, res, video: metadata, domain});
+            else return res.render('watch', {
+                title: `${metadata.title} | ${res.locals.site.title}`,
+                res,
+                video: metadata,
+                domain
+            });
 
         } catch (e) {
             return res.send('Invalid video URL');
@@ -46,7 +54,7 @@ router.get('/watch', function(req, res, next) {
 })
 
 /* GET new video page - this is where the user can request new videos be downloaded */
-router.get('/newVideo', function(req, res, next) {
+router.get('/newVideo', function (req, res, next) {
 
     // Parameters
     // - ?v = the video URL / ID
@@ -60,31 +68,35 @@ router.get('/newVideo', function(req, res, next) {
             const metadata = require(`../data/metadata/${videoId}/metadata.json`);
 
             if (metadata.downloaded) return res.redirect(`/video/${videoId}`);
-            else return res.render('newVideo', { title: `${res.locals.site.title} | New Video`, res, video: metadata });
+            else return res.render('newVideo', {title: `${res.locals.site.title} | New Video`, res, video: metadata});
 
         } catch (e) {
             return res.send('Invalid video URL');
         }
     } else {
-        res.render('newVideo', { title: `${res.locals.site.title} | New Video`, res, video: req.query.v });
+        res.render('newVideo', {title: `${res.locals.site.title} | New Video`, res, video: req.query.v});
     }
 });
 
 /* GET /search */
-router.get('/search', function(req, res, next) {
+router.get('/search', async function (req, res, next) {
 
     // Parameters
     // - ?q = the search query
+    // - ?am = the amount of results to return, defaults to 15, max is 50
 
     // If the q param is specified
     if (req.query.q) {
 
         // Get the search query
         let searchQuery = req.query.q;
+        let searchAm = req.query.am || 15;
+
+        if (searchAm > 50) searchAm = 50;
+
         try {
 
             // For each video in videos/
-            const fs = require('fs');
             const videos = fs.readdirSync(config.storage.paths.videos);
             let videoData = [];
 
@@ -96,17 +108,35 @@ router.get('/search', function(req, res, next) {
             });
 
             // Filter the videos based on the search query
-            const Fuse = require('fuse.js');
             const options = {
                 includeScore: true,
                 keys: ['title', 'description', 'channel']
             };
 
             const fuse = new Fuse(videoData, options);
-            const result = fuse.search(searchQuery);
+            const localVideos = fuse.search(searchQuery);
+
+            // Search YouTube
+            const ytVideos = await youtubedl(`ytsearch${searchAm}:${searchQuery}`, {
+                dumpSingleJson: true,
+                noWarnings: true,
+                noCallHome: true,
+                preferFreeFormats: false,
+                youtubeSkipDashManifest: true,
+                referer: 'https://screw.big.tech/',
+                geoBypass: true,
+                noCheckCertificate: true,
+                output: `${config.storage.paths.metadata}/metadata.json`
+            });
 
             // Render the search results
-            res.render('search', { title: `${res.locals.site.title} | Search`, res, videos: result, query: searchQuery });
+            res.render('search', {
+                title: `${res.locals.site.title} | Search`,
+                res,
+                localVideos,
+                ytVideos: ytVideos.entries,
+                query: searchQuery
+            });
 
         } catch (e) {
             console.log(e);
