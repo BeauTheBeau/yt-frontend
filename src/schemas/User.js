ed8c66d48d19a78fb02bed8801df1a9a631c29ae
beauthebeau <beauthebeau@proton.me>
2024-01-21 13:15:08 +0000

    feat: Update user schema to automatically salt password on change

--- src/schemas/User.js
+++ src/schemas/User.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
+
+const config = require("./../../config");
 
 const UserSchema = new Schema({
 
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    handle: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false },
 
     watched: { type: [{ type: Schema.Types.ObjectId, ref: 'Video' }], default: [] },
     liked: { type: [{ type: Schema.Types.ObjectId, ref: 'Video' }], default: [] },
@@ -14,5 +19,31 @@ const UserSchema = new Schema({
     playlists: { type: [{ type: Schema.Types.ObjectId, ref: 'Playlist' }], default: [] },
 
 });
+
+UserSchema.pre('save', function (next) {
+
+    const user = this;
+    if (!user.handle) user.handle = user.username.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+
+    });
+
+    next();
+
+});
+
+UserSchema.methods.generateJWT = function () {
+    const payload = { id: this._id, username: this.username };
+    return jwt.sign(payload, config.jwt.secret, {expiresIn: config.jwt.expiresIn});
+};
 
 module.exports = mongoose.model('User', UserSchema);
